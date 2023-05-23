ab555b36fa4f210bdf45ddf4c4d1f6e12daf1d64
mr.ritik99@gmail.com <mr.ritik99@gmail.com>
2023-05-23 10:20:38 +0530

    new version of node added

--- scripts/codedeploy_before_install.sh
+++ scripts/codedeploy_before_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Install node.js and PM2 globally
 sudo apt-get update
-sudo apt-get install nodejs-legacy -y
-sudo apt-get install npm  -y
+sudo apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
 sudo npm install pm2 -g
 sudo rm -rf /home/ubuntu/my-app1
