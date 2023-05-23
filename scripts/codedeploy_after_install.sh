bf95bcce89b146693c2e0c219201db8553f96ff3
mr.ritik99@gmail.com <mr.ritik99@gmail.com>
2023-05-23 13:53:40 +0530

    permission to 777

--- scripts/codedeploy_after_install.sh
+++ scripts/codedeploy_after_install.sh
@@ -2,6 +2,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-sudo chmod -R 775 /home/ubuntu/my-app1
+sudo chmod -R 777 /home/ubuntu/my-app1
 cd /home/ubuntu/my-app1/server
 npm install --omit=dev
