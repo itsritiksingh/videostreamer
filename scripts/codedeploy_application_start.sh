df4c85f7d7085c70684d9a33c758a2eaf780361e
mr.ritik99@gmail.com <mr.ritik99@gmail.com>
2023-05-23 13:48:53 +0530

    npm and pm2 not found fixed

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Stop all servers and start the server
+export HOME="/home/ubuntu/"
+sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
 pm2 stop all
 pm2 start /home/ubuntu/my-app1/server/index.js
