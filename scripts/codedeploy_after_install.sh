746acd7bca8e41899718e16967182ed9493ac35b
mr.ritik99@gmail.com <mr.ritik99@gmail.com>
2023-05-23 14:01:29 +0530

    afterstart deleted

--- scripts/codedeploy_after_install.sh
+++ scripts/codedeploy_after_install.sh
@@ -4,4 +4,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 sudo chmod -R 777 /home/ubuntu/my-app1
 cd /home/ubuntu/my-app1/server
+export HOME="/home/ubuntu/"
+sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
 npm install --omit=dev
+pm2 stop all
+pm2 start /home/ubuntu/my-app1/server/index.js
--- scripts/codedeploy_application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Stop all servers and start the server
-export HOME="/home/ubuntu/"
-sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
-pm2 stop all
-pm2 start /home/ubuntu/my-app1/server/index.js
